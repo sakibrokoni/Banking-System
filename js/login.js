c93a5ac5573ea559333e1e3eb7e757c41e73fc34
Sakib Rokoni <sakib15-12961@diu.edu.bd>
2021-08-20 04:26:41 +0600

    Done

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // check email and password
+    if (userEmail == 'sakib@gmail.com' && userPassword == 'Testing') {
+        window.location.href = 'banking.html';
+    }
+});
